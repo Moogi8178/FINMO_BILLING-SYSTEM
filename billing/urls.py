aae8cc37a115266f5fc43334acc3a3d6713264c6
Moogi8178 <finleymagembe@gmail.com>
2026-08-09 17:42:11 +0300

    Register ProviderViewSet with the router

--- billing/urls.py
+++ billing/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
+router.register('providers', views.ProviderViewSet)
 router.register('packages', views.PackageViewSet)
 router.register('customers', views.CustomerViewSet)
 router.register('invoices', views.InvoiceViewSet)
