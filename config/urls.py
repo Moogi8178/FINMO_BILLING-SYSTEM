756ad072ff17c080a2cc0b137d47dd3a4a1afb2e
Moogi8178 <finleymagembe@gmail.com>
2026-08-09 18:29:53 +0300

    Add purchase page URL to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from billing import views as billing_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('billing.urls')),
+    path('pay/<slug:slug>/', billing_views.purchase_page, name='purchase-page'),
 ]
