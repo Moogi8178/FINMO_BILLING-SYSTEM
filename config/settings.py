00f58889744658858b2aa66b895d31c8b5ecb1d4
Moogi8178 <finleymagembe@gmail.com>
2026-08-14 07:56:51 +0300

    Add Gmail SMTP settings for email notifications
    Configured email settings for password reset using Gmail SMTP.

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,15 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
 
+# ---- Email (for password reset) - Gmail SMTP ----
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
